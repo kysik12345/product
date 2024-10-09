435cd032965fa53086b633969bf5f44ff5e9f162
kysik12345 <kysik12345@mail.ru>
2024-10-09 21:44:18 +0300

    new

---
+++ djangoShop/shop/myshop/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import (ListView, CreateView, UpdateView, DetailView, DeleteView)
+from .models import Product
+class ProductListView(ListView):
+    model = Product
+    template_name = 'shop/products.html'
+    context_object_name = 'products'
+
+
